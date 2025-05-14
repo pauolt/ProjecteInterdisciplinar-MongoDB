231f2afb1b07541e557ee83fd8dc3fef0728e3d9
pauolt <pauolt2@alu.edu.gva.es>
2025-05-14 13:01:09 +0200

    Initial Commit.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json('API REST con Express y Knex');
 });
 
 module.exports = router;
